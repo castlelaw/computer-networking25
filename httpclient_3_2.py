5d9485b0ddd1f12eaa0973d1ec585b4fd48bd808
castlelaw <c20944@kma.ac.kr>
2025-04-06 19:16:37 +0900

    응답관련

--- httpclient_3_2.py
+++ httpclient_3_2.py
@@ -28,44 +28,55 @@ def send_request(url):
 
 
 def send_http_request(host, port, path):
-   try: 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((host, port))
+    
+   
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
 
 
-        request = f"GET {path} HTTP/1.0\r\nHost: {host}\r\nConnection: close\r\n\r\n"
-        client_socket.sendall(request.encode())
+    request = f"GET {path} HTTP/1.0\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+    client_socket.sendall(request.encode())
 
-        response = b""
-        while True:
-          data = client_socket.recv(1024)
-          if not data:
-              break
-          response += data
-            
-        response_str = response.decode(errors='ignore')
-        headers, body = response_str.split("\r\n\r\n", 1)
-        status_line = headers.split("\r\n")[0]
-        status_code = int(status_line.split()[1])
+    response = b""
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        response += data
+
+    client_socket.close()
+    return response     
+       
+def sp_response(response):         
+      response_str = response.decode(errors='ignore')
+      headers, body = response_str.split("\r\n\r\n", 1)
+      status_line = headers.split("\r\n")[0]
+      status_code = int(status_line.split()[1])
         
-        print(f"Status: {status_code}")
-        
-        if status_code == 200:
-            print("Success, 200 OK")
-            sys.exit(0)
-        
-        if status_code >= 400:
-            print("Error: Received HTTP", status_code)
-            print(body)
-            sys.exit(1)
+      header_dic ={}
+      for header in headers.split("\r\n")[1:]:
+          if ":" in header:
+              key, value = header.split(':',1)
+              header_dic[key.strip().lower()] = value.strip()
+      return status_code, header_dic, body
+      
+      if status_code == 200:
+          print("Success, 200 OK")
+          sys.exit(0)
+      
+      if status_code >= 400:
+          print("Error: Received HTTP", status_code)
+          print(body)
+          sys.exit(1)
           
 
-def http_headers(header_str):
-    headers = {}
-    line = header_str.split("\r\n")
-    for dic in line[1:]:
-        if ':' in dic:
-            key, value = dic.split(':', 1)
-            headers[key.strip().lower()] = value.strip()
-    return headers
+
+
+
+if __name__=="__main__":
+    if len(sys.argv) != 2:
+        sys.stderr.write(f"Usage: phthon {sys.argv[0]} http://example.com/page\n")
+        sys.exit(1)
+
+    
 
