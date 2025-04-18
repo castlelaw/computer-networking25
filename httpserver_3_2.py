6865c07f06b6319f6caaba81b545be44c3314c99
castlelaw <c20944@kma.ac.kr>
2025-04-18 23:45:28 +0900

    파일 존재

--- httpserver_3_2.py
+++ httpserver_3_2.py
@@ -1,6 +1,15 @@
 import sys
 import socket
 
+# 파일 존재 확인하기
+def file_exist(filename):
+    try:
+        f = open(filename, "r")  
+        f.close()
+        return True
+    except:
+        return False
+
 # HTTP 응답 메시지
 HTTP_200 = "HTTP/1.0 200 OK\r\n"
 HTTP_403 = "HTTP/1.0 403 Forbidden\r\n\r\n<html><body><h1>403 Forbidden</h1></body></html>"
